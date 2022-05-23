27e208cfde3928192d076bf7c3edd4a95f94fefe
aCube <player559001@gmail.com>
2022-05-22 22:27:44 -0300

    Set Linux Kernel code style; Remove redudant code; Improve some lines of code.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-#include"raylib/src/raylib.h"
 #include<vector>
+
+#ifdef _WIN32
+#include "raylib/src/raylib.h"
+#elif __linux__
+#include "raylib.h"
+#endif
+
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 #define WINDOW_TITLE "Development"
@@ -16,16 +22,16 @@ bool InGame = false;
 bool InDecks = false;
 bool isDeckLoop = false;
 bool ChosingDeck = false;
-Camera2D camera = { 0 };
+Camera2D camera = {0};
 
-
-class Card{
+class Card {
 public:
     std::string name;
-    int Id;
-    std::vector< int > Attacks;
     std::string fileName;
-    void SetCard(std::string name, int Id, int atk1, int atk2, std::string fileNameFunc){
+    std::vector<int> Attacks;
+    int Id;
+    Card(std::string name, int Id, int atk1, int atk2, std::string fileNameFunc)
+    {
         name = name;
         Id = Id;
         Attacks.push_back(atk1);
@@ -36,134 +42,146 @@ public:
 std::vector<Card> AddToDeck;
 std::vector<Card> Cards;
 
-class game{
+class game {
 public:
-    void Draw(){
-        DrawText("In Game",300,300,33,WHITE);
+    void Draw() {
+        DrawText("In Game", 300, 300, 33, WHITE);
     }
 };
-class Camera_cfg{
+
+class Camera_cfg {
 private:
-    float x_target;
-    float y_target;
+    Vector2 target;
     float rotation;
     float zoom;
 public:
-    float GetXTarget(){ return x_target; }
-    float GetYTarget(){ return y_target; }
-    float GetRotation(){ return rotation; }
-    float GetZoom(){ return zoom; }
-    void SetXTarget(float element){ x_target = element; }
-    void SetYTarget(float element){ y_target = element; }
-    void SetRotation(float element){ rotation = element; }
-    void SetZoom(float element){ zoom = element; }
-    void AddX(float element){x_target = x_target + element;};
-    void RemoveX(float element){x_target = x_target - element;};
-    void AddY(float element){y_target = y_target + element;};
-    void RemoveY(float element){y_target = y_target - element;};
+    float GetXTarget() { return target.x; }
+    float GetYTarget() { return target.y; }
+    float GetRotation() { return rotation; }
+    float GetZoom() { return zoom; }
 
+    void SetXTarget(float element) { target.x = element; }
+    void SetYTarget(float element) { target.y = element; }
+    void SetRotation(float element) { rotation = element; }
+    void SetZoom(float element) { zoom = element; }
+    
+    void AddX(float element) { target.x += element;};
+    void RemoveX(float element) { target.x -= element;};
+    void AddY(float element) { target.y += element;};
+    void RemoveY(float element) {target.y -= element;};
 };
-class menuPrincipal{
+class menuPrincipal {
 private:
-    Rectangle play = {10,5,120,50};
-    Rectangle Decks = {145,5,120,50};
-    Rectangle botao2 = {145+135,5,120,50};
+    Rectangle play = {10, 5, 120, 50};
+    Rectangle Decks = {145, 5, 120, 50};
+    Rectangle botao2 = {145 + 135,5, 120, 50};
 public:
-    void Draw(){
-        DrawRectangle(play.x, play.y+20, play.width, play.height, WHITE);
-        DrawRectangle(Decks.x, Decks.y+20,Decks.width,Decks.height,WHITE);
-        DrawRectangle(botao2.x,botao2.y+20,botao2.width, botao2.height, WHITE);
-        DrawText("Play", 42,40,30,BLACK);
-        DrawText("Coleção", 147,40,30,BLACK);
-        DrawText("Otro", 287,40,30,BLACK);
+    void Draw()
+    {
+        DrawRectangle(play.x, play.y + 20, play.width, play.height, WHITE);
+        DrawRectangle(Decks.x, Decks.y + 20, Decks.width, Decks.height, WHITE);
+        DrawRectangle(botao2.x,botao2.y + 20, botao2.width, botao2.height, WHITE);
+        /* Draw Options */
+        DrawText("Play", 42, 40, 30, BLACK);
+        DrawText("Coleção", 147, 40, 30, BLACK);
+        DrawText("Otro", 287, 40, 30,BLACK);
 
     }
-    void Listener(){
-            if(CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), Decks) && IsMouseButtonPressed(0)){
+
+    void Listener()
+    {
+            if (CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), Decks)
+            && IsMouseButtonPressed(0)) {
                     InDecks = true;
                     isDeckLoop=true;
                     InMenu=false;
                     InGame=false;
             }
-            if(CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(),camera), play) && IsMouseButtonPressed(0)){
+            if (CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), play)
+            && IsMouseButtonPressed(0)) {
                 InGame = true;
                 InMenu = false;
                 InDecks = false;
-                };
+            }
     };
 };
-class deck{
+
+class deck {
 public:
     std::string Name;
-    int DeckId;
     std::vector<Card> deck;
+    int DeckId;
     void SetDeck(std::string DeckName, std::vector<Card> DeckCards, long double Id){
         deck = DeckCards;
         DeckId = Id;
     }
 };
-//Setting player inventory !!!
 
-//Create the player deck list. This will be modified when the games has database
+/* Setting player inventory !!! */
+
+/* Create the player deck list. This will be modified when the games has database */
 std::vector<deck> PlayerDecks;
 
-
-class deckscreen{
+class deckscreen {
 private:
-    //Create the button rectangles
-    Rectangle FinalizeButton = {500,500,140,40};
-    Rectangle CreateButton = {790,60,100,60};
+    /* Create the button rectangles */
+    Rectangle FinalizeButton = {500, 500, 140, 40};
+    Rectangle CreateButton = {790, 60, 100, 60};
 public:
-    void Draw(){
-        DrawText("+", CreateButton.x,CreateButton.y,50,WHITE);
-        if(ChosingDeck){
-            //Draw text to finalize the deck
+    void Draw()
+    {
+        DrawText("+", CreateButton.x, CreateButton.y, 50, WHITE);
+        if (ChosingDeck) {
+            /* Draw text to finalize the deck */
             DrawText("Finalize", 500, 500, 30, GRAY);
         }
     }
-    void Listener(){
-        if(CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), CreateButton) && IsMouseButtonPressed(0)){
+    void Listener()
+    {
+        if (CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), CreateButton)
+        && IsMouseButtonPressed(0)) {
             ChosingDeck = true;
         }
-        //Check if button to create deck was pressed
-        if(CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), FinalizeButton) && IsMouseButtonPressed(0)){
-            //Check if deck have the Min_Cards_Quantity
-            if(AddToDeck.size()>MIN_CARDS_IN_DECK){
-                //Create the deck
+
+        /* Check if button to create deck was pressed */
+        if (CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), FinalizeButton)
+        && IsMouseButtonPressed(0)) {
+            /* Check if deck have the Min_Cards_Quantity */
+            if (AddToDeck.size()>MIN_CARDS_IN_DECK){
+                /* Create the deck */
                 deck thisDeck;
-                //Set the deck info
+                /* Set the deck info */
                 thisDeck.SetDeck("Deck1", AddToDeck, PlayerDecks.size());
-                //Clean the cards will be added in deck
-                AddToDeck.clear();    
-                //Add the deck to player decklist            
+                /* Clean the cards will be added in deck */
+                AddToDeck.clear();
+                /* Add the deck to player decklist */
                 PlayerDecks.push_back(thisDeck);
                 printf("Deck added to wallet. This is your %ld deck", PlayerDecks.size());
             }
         }
     }
-
 };
 
-class button{
+class button {
 public:
-    //Drawing card buttons
+    /* Drawing card buttons */
     Rectangle button;
     std::vector<Rectangle> Rectangles;
     void Draw(float x, float y){
-        Rectangle button={x,y,50,50};
-        if(ChosingDeck){
-            DrawText("+",x,y,50,WHITE);
+        Rectangle button = {x, y, 50, 50};
+        if (ChosingDeck) {
+            DrawText("+",x, y, 50, WHITE);
         }
-        DrawRectangle(x,y,50,50,WHITE);
+        DrawRectangle(x, y, 50, 50, WHITE);
         Rectangles.push_back(button);
     }
     void Listener(){
-        int i = 0;
-        //Checking who card is pressed
-        if(IsMouseButtonPressed(0)){
-            for(i=0;i<Cards.size();i++){
-                if(CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(),camera), Rectangles[i]) && IsMouseButtonPressed(0)){
-                    if(ChosingDeck){
+        /* Checking who card is pressed */
+        if (IsMouseButtonPressed(0)) {
+            for (size_t i = 0; i < Cards.size(); ++i) {
+                if (CheckCollisionPointRec(GetScreenToWorld2D(GetMousePosition(), camera), Rectangles[i])
+                && IsMouseButtonPressed(0)) {
+                    if (ChosingDeck) {
                         AddToDeck.push_back(Cards[i]);
                         printf("Card Loaded sucessfully: %s\n", Cards[i].fileName.c_str());
                         printf("Deck cards: %ld\n", AddToDeck.size());
@@ -174,93 +192,103 @@ public:
     }
 };
 
+int main(void) {
+    /* Initialize Window */
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
 
-int main(){
-    //Window inicialization
-    InitWindow(WINDOW_WIDTH,WINDOW_HEIGHT,WINDOW_TITLE);
-
-    //Camera Config
+    /* Camera Config */
     Camera_cfg Camera;
     Camera.SetXTarget(20.0f);
     Camera.SetYTarget(20.0f);
     Camera.SetRotation(0.0f);
     Camera.SetZoom(1.0f);
-    camera.target = (Vector2){Camera.GetXTarget(),Camera.GetYTarget()};
+    camera.target = (Vector2) {Camera.GetXTarget(), Camera.GetYTarget()};
     camera.rotation = Camera.GetRotation();
     camera.zoom = Camera.GetZoom();
-    //Cards
-    Card Lion; Lion.SetCard("Lion", 0, 32,32,"card.png"); Cards.push_back(Lion);
-    Card raiko; raiko.SetCard("raiko", 1, 230,23,"card2.png"); Cards.push_back(raiko);
-    Card Ave; Ave.SetCard("Ave", 1, 230,23,"card2.png"); Cards.push_back(Ave);
-    Card Mula; Mula.SetCard("Mula", 1, 230,23,"card2.png"); Cards.push_back(Mula);
-    Card Dilma; Dilma.SetCard("Dilma", 1, 230,23,"card2.png"); Cards.push_back(Dilma);
 
-    //Classes
+    /* Cards */
+    auto Lion = Card("Lion", 0, 32, 32, "card.png");
+    auto Raiko = Card("raiko", 1, 230, 23, "card2.png");
+    auto Ave = Card("Ave", 1, 230, 23, "card2.png");
+    auto Mula = Card("Mula", 1, 230,23, "card2.png");
+    auto Dilma = Card("Dilma", 1, 230,23, "card2.png");
+    Cards.push_back(Lion);
+    Cards.push_back(Raiko);
+    Cards.push_back(Ave);
+    Cards.push_back(Mula);
+    Cards.push_back(Dilma);
+
+    /* Classes */
     menuPrincipal Menu;
     game Game;
     deck Deck;
     button Button;
     deckscreen DeckScreen;
-    //Texture Declaration
-    std::vector< Texture2D > CardList;
-    for(int i=0;i<Cards.size();i++){
+    /* Texture Declaration */
+    std::vector<Texture2D> CardList;
+    for (size_t i = 0; i < Cards.size(); ++i) {
         Image thisCarde = LoadImage(Cards[i].fileName.c_str());
-        ImageResize(&thisCarde,150,150);
+        ImageResize(&thisCarde, 150, 150);
         Texture2D thisCard = LoadTextureFromImage(thisCarde);
         CardList.push_back(thisCard);
     }
+
     Image BkScreen = LoadImage("biosystem-tcg.png");
     ImageResize(&BkScreen, WINDOW_WIDTH, WINDOW_HEIGHT);
     Texture2D BackScreen = LoadTextureFromImage(BkScreen);
     UnloadImage(BkScreen);
-    
 
-    while(!WindowShouldClose()){
+    while(!WindowShouldClose()) {
         BeginDrawing();
             BeginMode2D(camera);
-                //Drawing FPS
                 DrawFPS(600,40);
-                //Decks listener
-                if(InDecks){Button.Listener(); DeckScreen.Listener();}
-                //Camera Updates
-                camera.target = (Vector2){Camera.GetXTarget(),Camera.GetYTarget()};
-                Camera.AddY(GetMouseWheelMove()* 100);
-                //Drawing DeckScreen
-                if(InDecks){DeckScreen.Draw();
+
+                /* Decks listener */
+                if (InDecks) { Button.Listener(); DeckScreen.Listener(); }
+                /* Update Camera */
+                camera.target = (Vector2) {Camera.GetXTarget(), Camera.GetYTarget()};
+                Camera.AddY(GetMouseWheelMove() * 100);
+
+                /* Drawing DeckScreen */
+                if (InDecks)
+                    DeckScreen.Draw();
+
+                /* Drawing Menu */
+                if (InMenu) {
+                    DrawTexture(BackScreen, 0, 0, WHITE);
+                    Menu.Draw();
                 }
-                //Drawing Menu
-                if(InMenu){
-                    DrawTexture(BackScreen, 0,0,WHITE);
-                    Menu.Draw(); 
-                    }
-                //Listening to menu
-                if(InMenu){
+
+                /* Listening to menu */
+                if (InMenu)
                     Menu.Listener();
-                }
-                //Drawing Game
-                if(InGame){
+
+                /* Drawing Game */
+                if (InGame)
                     Game.Draw();
-                    }
-                //Drawing Decks Menu cards
-                if(InDecks&&isDeckLoop){
-                    for (int l=0; l<MAX_CARDS_ROWS;l++){
-                        for(int c=0;c<MAX_CARDS_COLUMNS;c++){
-                        DrawTexture(CardList[c],20+l*110,c*130,WHITE);
-                        Button.Draw(20+l*110,c*130);
+
+                /* Drawing Decks Menu cards */
+                if (InDecks) {
+                    if (isDeckLoop) {
+                        for (size_t l = 0; l < MAX_CARDS_ROWS; ++l) {
+                            for (size_t c = 0; c < MAX_CARDS_COLUMNS; ++c) {
+                                DrawTexture(CardList[c], 20 + l * 110,c * 130, WHITE);
+                                Button.Draw(20 + l * 110, c * 130);
+                            }
                         }
-                    }
-                }
-                if(InDecks){
-                    for (int l=0; l<MAX_DECKS_ROWS;l++){
-                        for(int c=0;c<MAX_DECKS_COLUMNS;c++){
-                            for(int i=0; i<PlayerDecks.size();i++){
-                                DrawRectangle(600+l*110,i*130,100,120,WHITE);
+                    } else {
+                        for (size_t l = 0; l < MAX_DECKS_ROWS; ++l) {
+                            for (size_t c = 0; c < MAX_DECKS_COLUMNS; ++c) {
+                                for (size_t i = 0; i < PlayerDecks.size(); ++i) {
+                                    DrawRectangle(600 + l * 110, i * 130, 100, 120, WHITE);
+                                }
                             }
                         }
                     }
                 }
+
                 ClearBackground(BLACK);
             EndMode2D();
         EndDrawing();
-                
-                }}
+    }
+}
